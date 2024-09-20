96f9a4e03677d5fc516c938649a2f360321a3059
Athul S <athul162004@gmail.com>
2024-09-20 21:19:20 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
 
 /*====== typed js =========*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Devoloper', 'Embedded Devoloper'],
+    strings: ['WEB Devoloper', 'Embedded Devoloper'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
